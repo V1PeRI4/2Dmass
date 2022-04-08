a779e542b6c9fece2ea65fc4da44cd1b2019a407
user <user@C1005>
2022-04-08 09:49:18 +0300

    Добавьте файлы проекта.

---
+++ 2Dmass/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2Dmass
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            public int[,] massive = new int[3, 5]
+            {
+                {2, 4 , 5, 6, 7},
+                {3, 5, 7, 3, 2 },
+                {3, 7, 4, 2, 4 },
+            };
+
+            public int[,] massive2 = new int[3, 5]
+            {
+                {2, 4 , 5, 6, 7},
+                {3, 5, 7, 3, 2 },
+                {3, 7, 4, 2, 4 },
+
+            };
+        }
+    }
+}
